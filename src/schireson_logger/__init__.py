450e09781106ec8b8cfd543ec6b6b4d33c09eb12
Marlon Merjos <marlon@schireson.com>
2019-02-13 10:52:03 -0500

    Exposed the create_log_handler function to be importable

--- src/schireson_logger/__init__.py
+++ src/schireson_logger/__init__.py
@@ -1,2 +1,2 @@
 from schireson_logger.decorator import log_exceptions  # noqa
-from schireson_logger.logger import log, setup_logging  # noqa
+from schireson_logger.logger import create_log_handler, log, setup_logging  # noqa
